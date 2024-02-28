4f073968ccbe0626574426e20ba27a276d74d440
Zephyr@home <zephyr1125@gmail.com>
2024-02-28 17:34:11 +0800

    实现到获得FaceMesh的Landmark数据

---
+++ Assets/MyTests/Scripts/FaceMesh.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf73fe6eaf38961692a4772a9128b0b1ebeefae99bb75376f0158992d1233aa3
+size 4720
---
+++ Assets/MyTests/Scripts/HelloWorld.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:815a93481f5bb536db0807c20f7192162cd4b5510d1fdf75bcea604836ba60c4
+size 1259
