480126ef3855a5eafca2b87b9fde8ca27a709152
Zephyr@home <zephyr1125@gmail.com>
2024-03-04 16:46:02 +0800

    复制过来的Solution和Graph可以正常工作

---
+++ Assets/MyTests/Scripts/MyPoseTrackingGraph.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7bafffba5bf2b99ac658e1fdd73a421203191d17e8af90e55ee390f41a13411
+size 10698
---
+++ Assets/MyTests/Scripts/MyPoseTrackingSolution.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:335dc775aedf7f70c7b447f5b3e001a6c958162a345b443b7b3f32fe9297b227
+size 5229
